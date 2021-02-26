33d27632780166e547b3eb3e8321d1fe56e2fe5e
James Abel <j@abel.co>
2021-02-25 22:16:20 -0800

    modernize the venv creation

--- make_venv.bat
+++ make_venv.bat
@@ -3,6 +3,6 @@ rmdir /S /Q venv
 "\Program Files\Python38\python.exe" -m venv --clear venv
 call venv\Scripts\activate.bat
 python -m pip install --no-deps --upgrade pip
-pip3 install -U setuptools
-pip3 install -U -r requirements-dev.txt
+pip install -U setuptools
+pip install -U -r requirements-dev.txt
 call deactivate
