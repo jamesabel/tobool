159fea578e6646767b84de166bf12f636377f9ab
James Abel <j@abel.co>
2021-03-15 18:28:06 -0700

    add coverage.xml

--- pytest.bat
+++ pytest.bat
@@ -1,4 +1,3 @@
-call local_install.bat
 set PYTHONPATH=%CD%
-venv\Scripts\pytest.exe --rootdir="." -s test_tobool
+venv\Scripts\pytest.exe --rootdir="." -s test_tobool --cov-report xml:coverage.xml --cov=.\
 set PYTHONPATH=
