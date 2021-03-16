1ad5a71c4400a58d6e714937bdca75d3e45ea2d1
James Abel <j@abel.co>
2021-03-15 18:32:23 -0700

    direct pytest to just tobool

--- pytest.bat
+++ pytest.bat
@@ -1,3 +1,3 @@
 set PYTHONPATH=%CD%
-venv\Scripts\pytest.exe --rootdir="." -s test_tobool --cov-report xml:coverage.xml --cov=.\
+venv\Scripts\pytest.exe --rootdir="." -s test_tobool --cov-report xml:coverage.xml --cov-report html --cov=.\tobool
 set PYTHONPATH=
