457360354a770b7bac0c7dfed0b76cf569bb6058
James Abel <j@abel.co>
2022-06-11 11:01:13 -0700

    add typing and to_bool_strict

--- test_tobool/test_to_bool.py
+++ test_tobool/test_to_bool.py
@@ -1,22 +1,40 @@
 import pytest
+from copy import deepcopy
 
 from decimal import Decimal
 
-from tobool import to_bool
+from tobool import to_bool, to_bool_strict
+
+test_values = {
+    False: [False, "F", "false", "False", "FALSE", "F", "N", "no", 0, Decimal(0), Decimal(0.0), "off", " f"],
+    True: [True, "T", "true", "yes", "Y", 1, Decimal(1), Decimal(1.0), "on", "   T   ", "  t "],
+    None: [None, "None", "NULL", "null", " None", "nil", "na", "NA"],
+}
 
 
 def test_type_conversion():
-    test_values = {
-        False: [False, "F", "false", "False", "FALSE", "F", "N", "no", 0, Decimal(0), Decimal(0.0), "off", " f"],
-        True: [True, "T", "true", "yes", "Y", 1, Decimal(1), Decimal(1.0), "on", "   T   ", "  t "],
-        None: [None, "None", "NULL", "null", " None", "nil", "na", "NA"],
-    }
     for value, variants in test_values.items():
         for variant in variants:
-            assert value == to_bool(variant)
+            new_value = to_bool(variant)
+            assert type(new_value) is bool or new_value is None
+            assert value == new_value
 
     test_unsupported_values = {False: ["Nope", -1, "Nada", "fuggedaboutit", Decimal(-1)], True: ["Yup", "Si", 1.1, Decimal(100.0), Decimal(0.5)], None: ["huh?", "no idea", "nah", ""]}
     for value, variants in test_unsupported_values.items():
         for variant in variants:
             with pytest.raises(ValueError):
                 to_bool(variant)
+
+
+def test_strict():
+
+    # modify the "regular" test set and make all the None values False
+    strict_test_values = deepcopy(test_values)
+    strict_test_values[False].extend(test_values[None])
+    del strict_test_values[None]
+
+    for value, variants in strict_test_values.items():
+        for variant in variants:
+            new_value = to_bool_strict(variant)
+            assert type(new_value) is bool
+            assert value == new_value
--- tobool/tobool.py
+++ tobool/tobool.py
@@ -8,18 +8,20 @@ def to_bool(value: Union[str, bool, int, Decimal, None]) -> Union[bool, None]:
     performs a casting of a multitude of values to bool.
     i.e. "true", "TRUE", "y", "Yes", "on", "1", 1, "false", "FALSE", "n", "No", "off", "0", 0, etc.
     :param value: input value
-    :return: boolean value of original string
+    :return: boolean value of original value
     """
 
-    if type(value) == bool:
+    new_bool: Union[bool, None]  # for mypy
+    if isinstance(value, bool):
+        # bool has to come first since bools are also ints (i.e., isinstance(True, int) == True)
         new_bool = value
     elif value is None:
         new_bool = None
-    elif type(value) == int and 0 <= value <= 1:
+    elif isinstance(value, int) and 0 <= value <= 1:
         new_bool = bool(value)
-    elif type(value) == Decimal and (value == Decimal(0) or value == Decimal(1)):
+    elif isinstance(value, Decimal) and (value == Decimal(0) or value == Decimal(1)):
         new_bool = bool(value)
-    elif type(value) == str:
+    elif isinstance(value, str):
         value = value.strip().lower()
         if value == "none" or value == "null" or value == "nil" or value == "na":
             new_bool = None
@@ -29,4 +31,16 @@ def to_bool(value: Union[str, bool, int, Decimal, None]) -> Union[bool, None]:
     else:
         raise ValueError(value)
 
+    return new_bool
+
+
+def to_bool_strict(value: Union[str, bool, int, Decimal, None]) -> bool:
+    """
+    Like to_bool(), except the return is strictly only a bool. A None return becomes a False. Good for strict typing and not having to worry about Python's "truthiness".
+    :param value: input value
+    :return: boolean value of original value
+    """
+    new_bool = to_bool(value)
+    if new_bool is None:
+        new_bool = False
     return new_bool
