cf36cb69be1a9628c514c2be37d0a6077aa1fb85
James Abel <j@abel.co>
2022-06-11 21:37:12 -0700

    add ToBoolNoneValueException

--- test_tobool/test_to_bool.py
+++ test_tobool/test_to_bool.py
@@ -3,12 +3,12 @@ from copy import deepcopy
 
 from decimal import Decimal
 
-from tobool import to_bool, to_bool_strict
+from tobool import to_bool, to_bool_strict, ToBoolNoneValueException
 
 test_values = {
     False: [False, "F", "false", "False", "FALSE", "F", "N", "no", 0, Decimal(0), Decimal(0.0), "off", " f"],
     True: [True, "T", "true", "yes", "Y", 1, Decimal(1), Decimal(1.0), "on", "   T   ", "  t "],
-    None: [None, "None", "NULL", "null", " None", "nil", "na", "NA"],
+    None: [None, "None", "NONE", "NULL", "null", " None", "nil", "na", "NA"],
 }
 
 
@@ -38,3 +38,16 @@ def test_strict():
             new_value = to_bool_strict(variant)
             assert type(new_value) is bool
             assert value == new_value
+
+
+def test_strict_exception():
+
+    # test that ToBoolNoneValueException is raises on any "None" value
+    for value, variants in test_values.items():
+        if value is None:
+            with pytest.raises(ToBoolNoneValueException):
+                to_bool_strict(value, True)
+        else:
+            result = to_bool_strict(value, True)
+            assert isinstance(result, bool)
+            assert type(result) == bool
--- tobool/tobool.py
+++ tobool/tobool.py
@@ -3,6 +3,10 @@ import distutils.util
 from typing import Union
 
 
+class ToBoolNoneValueException(Exception):
+    ...
+
+
 def to_bool(value: Union[str, bool, int, Decimal, None]) -> Union[bool, None]:
     """
     performs a casting of a multitude of values to bool.
@@ -34,13 +38,19 @@ def to_bool(value: Union[str, bool, int, Decimal, None]) -> Union[bool, None]:
     return new_bool
 
 
-def to_bool_strict(value: Union[str, bool, int, Decimal, None]) -> bool:
+def to_bool_strict(value: Union[str, bool, int, Decimal, None], fault_on_none: bool = False) -> bool:
     """
-    Like to_bool(), except the return is strictly only a bool. A None return becomes a False. Good for strict typing and not having to worry about Python's "truthiness".
+    Like to_bool(), except the return is strictly only a bool (the "strict" is on the output). A None return becomes a False.
+    Good for strict return typing and not having to worry about Python's "truthiness" on a None.
+    If `fault_on_none` is set to True the input is also strict (any of the "None" values will raise a ToBoolNoneValueException).
     :param value: input value
+    :param fault_on_none: set to True to fault if any of the "None" values are passed (False to convert a None to False, which is the default), i.e. be strict on input
     :return: boolean value of original value
     """
     new_bool = to_bool(value)
     if new_bool is None:
-        new_bool = False
+        if fault_on_none:
+            raise ToBoolNoneValueException(value)
+        else:
+            new_bool = False
     return new_bool
